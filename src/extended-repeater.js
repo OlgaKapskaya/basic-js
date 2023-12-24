783e1764ecacc0e54641bec421c0eaf3cf4cbbbf
OlgaKapskaya <olikbuko@gmail.com>
2023-12-24 14:35:46 +0300

    feat: added repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,70 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
+ *
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+    let result = '';
+    const separator = options?.separator || "+";
+    const additionStr = getAdditionalStr(options);
+
+    if (options?.repeatTimes) {
+        for (let i = 0; i < options?.repeatTimes; i++) {
+            if (i !== options.repeatTimes - 1) {
+                result += `${str}${additionStr}${separator}`;
+            } else {
+                result += `${str}${additionStr}`
+            }
+        }
+    } else {
+        result = `${str}${additionStr}`;
+    }
+
+    return result;
+}
+
+function getAdditionalStr(options) {
+    let additionalStr = '';
+    const addition = (typeof options?.addition === 'boolean' || `${options?.addition}` === 'null')
+        ? `${options?.addition}`
+        : (`${options?.addition}` === 'undefined')
+            ? ''
+            : options?.addition;
+
+    const separator = !options?.additionRepeatTimes
+        ? ''
+        : options?.additionSeparator
+            ? options?.additionSeparator
+            : '|';
+
+
+    if (addition) {
+        if (options?.additionRepeatTimes) {
+            for (let i=0; i<options.additionRepeatTimes; i++) {
+                if (i !== options.additionRepeatTimes - 1) {
+                    additionalStr += `${options.addition}${separator}`;
+                } else {
+                    additionalStr += `${options.addition}`;
+                }
+            }
+        } else {
+            additionalStr = `${options.addition}`;
+        }
+    }
+
+    return additionalStr
 }
 
 module.exports = {
-  repeater
+    repeater
 };
