304470f6260362e6c87f93e54fcc312b56e34e3d
OlgaKapskaya <olikbuko@gmail.com>
2023-12-24 16:38:07 +0300

    feat: added isMAC48Address, getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Extract season from given date and expose the enemy scout!
  * 
@@ -11,9 +9,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date) {
+    if (date !== 'Invalid Date' && isDateValid(date)) {
+      const month = date.getMonth();
+      switch (month) {
+        case 0:
+        case 1:
+        case 11:
+          return 'winter';
+        case 2:
+        case 3:
+        case 4 :
+          return 'spring';
+        case 5:
+        case 6:
+        case 7 :
+          return 'summer';
+        case 8:
+        case 9:
+        case 10 :
+          return 'autumn';
+        default: return null
+      }
+    } else {
+      throw new Error('Invalid date!')
+    }
+  } else {
+    return 'Unable to determine the time of year!'
+  }
+}
+
+function isDateValid(date) {
+  return typeof date !== 'number' && !isNaN(new Date(date));
 }
 
 module.exports = {
